8210e0b7be1b8adf806bee5204071cc461f0364b
Дмитрий Севрюков <sevryukov.d2003@gmail.com>
2021-11-21 13:44:07 +0300

    В MainPage.xaml.cs добавил новый код

--- EntryTutorial/MainPage.xaml.cs
+++ EntryTutorial/MainPage.xaml.cs
@@ -16,5 +16,15 @@ namespace EntryTutorial
         {
             InitializeComponent();
         }
+        void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+        {
+            string oldText = e.OldTextValue;
+            string newText = e.NewTextValue;
+        }
+
+        void OnEntryCompleted(object sender, EventArgs e)
+        {
+            string text = ((Entry)sender).Text;
+        }
     }
 }
